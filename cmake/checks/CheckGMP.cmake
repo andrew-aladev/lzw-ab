cfcac96e4fb3f1dd4c4bc909322a53368edca4f0
Andrew Aladjev <aladjev.andrew@gmail.com>
2021-02-28 02:54:30 +0300

    implementing tommath bindings

--- cmake/checks/CheckGMP.cmake
+++ cmake/checks/CheckGMP.cmake
@@ -1,6 +1,6 @@
 set (CURRENT_LIST_DIR ${CMAKE_CURRENT_LIST_DIR})
 
-function (cmake_check_gmp)
+function (cmake_check_gmp CHECK_MODE)
   if (DEFINED CMAKE_GMP_WORKS)
     return ()
   endif ()
@@ -11,11 +11,6 @@ function (cmake_check_gmp)
   set (SOURCE_DIR "${CURRENT_LIST_DIR}/GMP")
   set (BINARY_DIR "${PROJECT_BINARY_DIR}/check_gmp")
 
-  find_package (GMP)
-
-  if (CMAKE_GMP_FOUND)
-  endif ()
-
   include (GetVerboseFlags)
   cmake_get_verbose_flags ()
 
@@ -25,32 +20,44 @@ function (cmake_check_gmp)
   include (CheckRunnable)
   cmake_check_runnable ()
 
-  try_compile (
-    COMPILE_RESULT ${BINARY_DIR} ${SOURCE_DIR} ${NAME}
-    CMAKE_FLAGS
-      "-DCMAKE_C_FLAGS=${CMAKE_VERBOSE_C_FLAGS} ${CMAKE_C18_C_FLAGS} ${CMAKE_WERROR_C_FLAGS}"
-      "-DCMAKE_VERBOSE_MAKEFILE=${CMAKE_VERBOSE_MAKEFILE}"
-      "-DCMAKE_GMP_INCLUDE_PATH=${CMAKE_GMP_INCLUDE_PATH}"
-      "-DCMAKE_GMP_SHARED_LIBRARY_PATH=${CMAKE_GMP_SHARED_LIBRARY_PATH}"
-      "-DCMAKE_GMP_STATIC_LIBRARY_PATH=${CMAKE_GMP_STATIC_LIBRARY_PATH}"
-      "-DCMAKE_TRY_RUN=${CMAKE_CAN_RUN_EXE}"
-    OUTPUT_VARIABLE COMPILE_OUTPUT
-  )
-  file (REMOVE_RECURSE ${BINARY_DIR})
+  find_package (GMP)
 
-  if (CMAKE_VERBOSE_MAKEFILE)
-    message (STATUS ${COMPILE_OUTPUT})
-  endif ()
+  if (CMAKE_GMP_FOUND)
+    try_compile (
+      COMPILE_RESULT ${BINARY_DIR} ${SOURCE_DIR} ${NAME}
+      CMAKE_FLAGS
+        "-DCMAKE_C_FLAGS=${CMAKE_VERBOSE_C_FLAGS} ${CMAKE_C18_C_FLAGS} ${CMAKE_WERROR_C_FLAGS}"
+        "-DCMAKE_VERBOSE_MAKEFILE=${CMAKE_VERBOSE_MAKEFILE}"
+        "-DCMAKE_GMP_INCLUDE_PATH=${CMAKE_GMP_INCLUDE_PATH}"
+        "-DCMAKE_GMP_SHARED_LIBRARY_PATH=${CMAKE_GMP_SHARED_LIBRARY_PATH}"
+        "-DCMAKE_GMP_STATIC_LIBRARY_PATH=${CMAKE_GMP_STATIC_LIBRARY_PATH}"
+        "-DCMAKE_TRY_RUN=${CMAKE_CAN_RUN_EXE}"
+      OUTPUT_VARIABLE COMPILE_OUTPUT
+    )
+    file (REMOVE_RECURSE ${BINARY_DIR})
 
-  if (COMPILE_RESULT)
-    set (CMAKE_GMP_WORKS true)
-    message (STATUS "${MESSAGE_PREFIX} - working")
+    if (CMAKE_VERBOSE_MAKEFILE)
+      message (STATUS ${COMPILE_OUTPUT})
+    endif ()
+
+    if (COMPILE_RESULT)
+      set (CMAKE_GMP_WORKS true)
+      message (STATUS "${MESSAGE_PREFIX} - working")
+    else ()
+      set (CMAKE_GMP_WORKS false)
+      message (STATUS "${MESSAGE_PREFIX} - not working")
+    endif ()
+
   else ()
     set (CMAKE_GMP_WORKS false)
-    message (FATAL_ERROR "${MESSAGE_PREFIX} - not working")
+    message (STATUS "${MESSAGE_PREFIX} - not working")
   endif ()
 
   set (CMAKE_GMP_WORKS ${CMAKE_GMP_WORKS} CACHE STRING "Status of GMP")
 
   mark_as_advanced (CMAKE_GMP_WORKS)
+
+  if (NOT CMAKE_GMP_WORKS AND CHECK_MODE STREQUAL "REQUIRED")
+    message (FATAL_ERROR "${MESSAGE_PREFIX} - GMP is required")
+  endif ()
 endfunction ()
--- cmake/checks/CheckTomMath.cmake
+++ cmake/checks/CheckTomMath.cmake
@@ -1,6 +1,6 @@
 set (CURRENT_LIST_DIR ${CMAKE_CURRENT_LIST_DIR})
 
-function (cmake_check_tommath)
+function (cmake_check_tommath CHECK_MODE)
   if (DEFINED CMAKE_TOMMATH_WORKS)
     return ()
   endif ()
@@ -11,11 +11,6 @@ function (cmake_check_tommath)
   set (SOURCE_DIR "${CURRENT_LIST_DIR}/TomMath")
   set (BINARY_DIR "${PROJECT_BINARY_DIR}/check_tommath")
 
-  find_package (TomMath)
-
-  if (CMAKE_TOMMATH_FOUND)
-  endif ()
-
   include (GetVerboseFlags)
   cmake_get_verbose_flags ()
 
@@ -25,32 +20,44 @@ function (cmake_check_tommath)
   include (CheckRunnable)
   cmake_check_runnable ()
 
-  try_compile (
-    COMPILE_RESULT ${BINARY_DIR} ${SOURCE_DIR} ${NAME}
-    CMAKE_FLAGS
-      "-DCMAKE_C_FLAGS=${CMAKE_VERBOSE_C_FLAGS} ${CMAKE_C18_C_FLAGS} ${CMAKE_WERROR_C_FLAGS}"
-      "-DCMAKE_VERBOSE_MAKEFILE=${CMAKE_VERBOSE_MAKEFILE}"
-      "-DCMAKE_TOMMATH_INCLUDE_PATH=${CMAKE_TOMMATH_INCLUDE_PATH}"
-      "-DCMAKE_TOMMATH_SHARED_LIBRARY_PATH=${CMAKE_TOMMATH_SHARED_LIBRARY_PATH}"
-      "-DCMAKE_TOMMATH_STATIC_LIBRARY_PATH=${CMAKE_TOMMATH_STATIC_LIBRARY_PATH}"
-      "-DCMAKE_TRY_RUN=${CMAKE_CAN_RUN_EXE}"
-    OUTPUT_VARIABLE COMPILE_OUTPUT
-  )
-  file (REMOVE_RECURSE ${BINARY_DIR})
+  find_package (TomMath)
 
-  if (CMAKE_VERBOSE_MAKEFILE)
-    message (STATUS ${COMPILE_OUTPUT})
-  endif ()
+  if (CMAKE_TOMMATH_FOUND)
+    try_compile (
+      COMPILE_RESULT ${BINARY_DIR} ${SOURCE_DIR} ${NAME}
+      CMAKE_FLAGS
+        "-DCMAKE_C_FLAGS=${CMAKE_VERBOSE_C_FLAGS} ${CMAKE_C18_C_FLAGS} ${CMAKE_WERROR_C_FLAGS}"
+        "-DCMAKE_VERBOSE_MAKEFILE=${CMAKE_VERBOSE_MAKEFILE}"
+        "-DCMAKE_TOMMATH_INCLUDE_PATH=${CMAKE_TOMMATH_INCLUDE_PATH}"
+        "-DCMAKE_TOMMATH_SHARED_LIBRARY_PATH=${CMAKE_TOMMATH_SHARED_LIBRARY_PATH}"
+        "-DCMAKE_TOMMATH_STATIC_LIBRARY_PATH=${CMAKE_TOMMATH_STATIC_LIBRARY_PATH}"
+        "-DCMAKE_TRY_RUN=${CMAKE_CAN_RUN_EXE}"
+      OUTPUT_VARIABLE COMPILE_OUTPUT
+    )
+    file (REMOVE_RECURSE ${BINARY_DIR})
 
-  if (COMPILE_RESULT)
-    set (CMAKE_TOMMATH_WORKS true)
-    message (STATUS "${MESSAGE_PREFIX} - working")
+    if (CMAKE_VERBOSE_MAKEFILE)
+      message (STATUS ${COMPILE_OUTPUT})
+    endif ()
+
+    if (COMPILE_RESULT)
+      set (CMAKE_TOMMATH_WORKS true)
+      message (STATUS "${MESSAGE_PREFIX} - working")
+    else ()
+      set (CMAKE_TOMMATH_WORKS false)
+      message (STATUS "${MESSAGE_PREFIX} - not working")
+    endif ()
+
   else ()
     set (CMAKE_TOMMATH_WORKS false)
-    message (FATAL_ERROR "${MESSAGE_PREFIX} - not working")
+    message (STATUS "${MESSAGE_PREFIX} - not working")
   endif ()
 
   set (CMAKE_TOMMATH_WORKS ${CMAKE_TOMMATH_WORKS} CACHE STRING "Status of TomMath")
 
   mark_as_advanced (CMAKE_TOMMATH_WORKS)
+
+  if (NOT CMAKE_TOMMATH_WORKS AND CHECK_MODE STREQUAL "REQUIRED")
+    message (FATAL_ERROR "${MESSAGE_PREFIX} - TomMath is required")
+  endif ()
 endfunction ()
