46dfd641d2beecb9f7afbe8b2a4de7fe015a5c5d
Andrew Aladjev <aladjev.andrew@gmail.com>
2021-04-02 21:02:37 +0300

    refactored export cmake scripts, cosmetics

--- PreLoad.cmake
+++ PreLoad.cmake
@@ -1,6 +1,11 @@
-if (WIN32)
+if (CYGWIN OR MSYS OR MINGW)
   set (
     CMAKE_GENERATOR "Unix Makefiles"
+    CACHE INTERNAL "Cmake generator" FORCE
+  )
+elseif (WIN32)
+  set (
+    CMAKE_GENERATOR "NMake Makefiles"
     CACHE INTERNAL "Cmake generator" FORCE
   )
 endif ()
--- cmake/checks/GetExportMode.cmake
+++ cmake/checks/GetExportMode.cmake
@@ -33,56 +33,113 @@ function (cmake_test_export_mode EXPORT_MODE_UPPERCASE EXPORT_SHARED_LIBRARY_LD_
   set (TEST_RESULT ${TEST_RESULT} PARENT_SCOPE)
 endfunction ()
 
-function (cmake_get_export_mode CHECK_MODE)
-  if (DEFINED CMAKE_EXPORT_MODE)
+function (cmake_get_export_all_flags EXPORT_MODE_UPPERCASE)
+  if (DEFINED CMAKE_HAVE_EXPORT_ALL)
     return ()
   endif ()
 
-  include (GetExportAllFlags)
-  cmake_get_export_all_flags ()
+  set (MESSAGE_PREFIX "Status of export all support")
 
-  if (WIN32 OR CYGWIN)
+  if (NOT MSVC)
+    set (
+      EXPORT_ALL_LD_FLAG_GROUPS
+      "-Wl,--export-all-symbols -Wl,--enable-auto-import"
+      "-fvisibility=default"
+      ""
+    )
+
+    foreach (EXPORT_ALL_LD_FLAGS ${EXPORT_ALL_LD_FLAG_GROUPS})
+      cmake_test_export_mode ("${EXPORT_MODE_UPPERCASE}" "${EXPORT_ALL_LD_FLAGS}")
+
+      if (TEST_RESULT)
+        set (CMAKE_HAVE_EXPORT_ALL true)
+        set (CMAKE_EXPORT_ALL_LD_FLAGS ${EXPORT_ALL_LD_FLAGS})
+
+        if (EXPORT_ALL_LD_FLAGS STREQUAL "")
+          set (EXPORT_ALL_LD_FLAGS "vanilla")
+        endif ()
+
+        message (STATUS "${MESSAGE_PREFIX} - ${EXPORT_ALL_LD_FLAGS}")
+        break ()
+      endif ()
+    endforeach ()
+
+    if (NOT TEST_RESULT)
+      set (CMAKE_HAVE_EXPORT_ALL false)
+      set (CMAKE_EXPORT_ALL_LD_FLAGS "")
+      message (STATUS "${MESSAGE_PREFIX} - no")
+    endif ()
+
+  else ()
+    set (CMAKE_HAVE_EXPORT_ALL false)
+    set (CMAKE_EXPORT_ALL_LD_FLAGS "")
+    message (STATUS "${MESSAGE_PREFIX} - no")
+  endif ()
+
+  set (
+    CMAKE_HAVE_EXPORT_ALL ${CMAKE_HAVE_EXPORT_ALL}
+    CACHE BOOL "Status of export all"
+  )
+  set (
+    CMAKE_EXPORT_ALL_LD_FLAGS ${CMAKE_EXPORT_ALL_LD_FLAGS}
+    CACHE STRING "Export all LD flags"
+  )
+
+  mark_as_advanced (CMAKE_HAVE_EXPORT_ALL CMAKE_EXPORT_ALL_LD_FLAGS)
+
+  set (TEST_RESULT ${CMAKE_HAVE_EXPORT_ALL} PARENT_SCOPE)
+endfunction ()
+
+function (cmake_get_export_mode CHECK_MODE)
+  set (MESSAGE_PREFIX "Status of export mode support")
+
+  if (DEFINED CMAKE_HAVE_EXPORT)
+    if (NOT CMAKE_HAVE_EXPORT AND CHECK_MODE STREQUAL "REQUIRED")
+      message (FATAL_ERROR "${MESSAGE_PREFIX} - is required")
+    endif ()
+
+    return ()
+  endif ()
+
+  if (WIN32 OR CYGWIN OR MSYS OR MINGW)
     set (EXPORT_MODES "dll" "vanilla")
   else ()
     set (EXPORT_MODES "visibility" "vanilla")
   endif ()
 
-  set (MESSAGE_PREFIX "Status of export mode support")
-
   foreach (EXPORT_MODE ${EXPORT_MODES})
     string (TOUPPER ${EXPORT_MODE} EXPORT_MODE_UPPERCASE)
 
     if (EXPORT_MODE STREQUAL "vanilla")
+      cmake_get_export_all_flags ("${EXPORT_MODE_UPPERCASE}")
       set (EXPORT_SHARED_LIBRARY_LD_FLAGS ${CMAKE_EXPORT_ALL_LD_FLAGS})
     else ()
+      cmake_test_export_mode ("${EXPORT_MODE_UPPERCASE}" "")
       set (EXPORT_SHARED_LIBRARY_LD_FLAGS "")
     endif ()
 
-    cmake_test_export_mode ("${EXPORT_MODE_UPPERCASE}" "${EXPORT_SHARED_LIBRARY_LD_FLAGS}")
-
     if (TEST_RESULT)
+      set (CMAKE_HAVE_EXPORT true)
       set (CMAKE_EXPORT_MODE ${EXPORT_MODE})
       set (CMAKE_EXPORT_MODE_UPPERCASE ${EXPORT_MODE_UPPERCASE})
       set (CMAKE_EXPORT_SHARED_LIBRARY_LD_FLAGS ${EXPORT_SHARED_LIBRARY_LD_FLAGS})
-
-      set (MESSAGE_TEXT "${MESSAGE_PREFIX} - ${EXPORT_MODE}")
-
-      if (NOT EXPORT_SHARED_LIBRARY_LD_FLAGS STREQUAL "")
-        set (MESSAGE_TEXT "${MESSAGE_TEXT}, shared library LD flags - ${EXPORT_SHARED_LIBRARY_LD_FLAGS}")
-      endif ()
-
-      message (STATUS ${MESSAGE_TEXT})
+      message (STATUS "${MESSAGE_PREFIX} - ${EXPORT_MODE}")
       break ()
     endif ()
   endforeach ()
 
   if (NOT TEST_RESULT)
+    set (CMAKE_HAVE_EXPORT false)
     set (CMAKE_EXPORT_MODE "")
     set (CMAKE_EXPORT_MODE_UPPERCASE "")
     set (CMAKE_EXPORT_SHARED_LIBRARY_LD_FLAGS "")
     message (STATUS "${MESSAGE_PREFIX} - no")
   endif ()
 
+  set (
+    CMAKE_HAVE_EXPORT ${CMAKE_HAVE_EXPORT}
+    CACHE BOOL "Status of export"
+  )
   set (
     CMAKE_EXPORT_MODE ${CMAKE_EXPORT_MODE}
     CACHE STRING "Export mode"
@@ -93,16 +150,17 @@ function (cmake_get_export_mode CHECK_MODE)
   )
   set (
     CMAKE_EXPORT_SHARED_LIBRARY_LD_FLAGS ${CMAKE_EXPORT_SHARED_LIBRARY_LD_FLAGS}
-    CACHE STRING "Export library LD flags"
+    CACHE STRING "Export shared library LD flags"
   )
 
   mark_as_advanced (
+    CMAKE_HAVE_EXPORT
     CMAKE_EXPORT_MODE
     CMAKE_EXPORT_MODE_UPPERCASE
     CMAKE_EXPORT_SHARED_LIBRARY_LD_FLAGS
   )
 
-  if (CMAKE_EXPORT_MODE STREQUAL "" AND CHECK_MODE STREQUAL "REQUIRED")
-    message (FATAL_ERROR "${MESSAGE_PREFIX} - export mode is required")
+  if (NOT CMAKE_HAVE_EXPORT AND CHECK_MODE STREQUAL "REQUIRED")
+    message (FATAL_ERROR "${MESSAGE_PREFIX} - is required")
   endif ()
 endfunction ()
