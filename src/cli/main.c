a760402f5463e1d9e0b93629fa3be88b65ac4d4f
Andrew Aladjev <aladjev.andrew@gmail.com>
2021-04-21 23:46:55 +0300

    implemented cli using argtable3 getopt

--- src/cli/main.c
+++ src/cli/main.c
@@ -2,7 +2,9 @@
 // Copyright (c) 2016 David Bryant, 2018+ other authors, all rights reserved (see AUTHORS).
 // Distributed under the BSD Software License (see LICENSE).
 
-#include <getopt.h>
+#define ARG_REPLACE_GETOPT 1
+
+#include <arg_getopt.h>
 #include <stdlib.h>
 
 #include "../file.h"
