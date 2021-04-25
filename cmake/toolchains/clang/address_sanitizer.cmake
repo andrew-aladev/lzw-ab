3b019355ab35fe173d80147d636f3c3705fc609b
Andrew Aladjev <aladjev.andrew@gmail.com>
2021-04-26 00:31:59 +0300

    fixed clang address sanitizer toolchain

--- cmake/toolchains/clang/address_sanitizer.cmake
+++ cmake/toolchains/clang/address_sanitizer.cmake
@@ -4,15 +4,7 @@ set (
   CMAKE_DEFAULT_C_FLAGS ${CMAKE_C_FLAGS}
   CACHE STRING "cmake default C flags"
 )
-
-if (CMAKE_GENERATOR MATCHES "Visual Studio")
-  set (
-    CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /fsanitize=address"
-    CACHE STRING "cmake C flags"
-  )
-else ()
-  set (
-    CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=address"
-    CACHE STRING "cmake C flags"
-  )
-endif ()
+set (
+  CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=address"
+  CACHE STRING "cmake C flags"
+)
