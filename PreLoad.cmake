04a07b4f57ff17ac396029b0ff2249b1232b09a0
Andrew Aladjev <aladjev.andrew@gmail.com>
2021-06-01 22:50:14 +0300

    improved preload cmake script for mingw

--- PreLoad.cmake
+++ PreLoad.cmake
@@ -1,6 +1,21 @@
-if ("$ENV{VisualStudioVersion}" STREQUAL "")
-  set (
-    CMAKE_GENERATOR "Unix Makefiles"
-    CACHE INTERNAL "Cmake generator"
-  )
+# Cmake uses Visual Studio generator for MinGW by default.
+# It will be better to select another default generator.
+if (NOT "$ENV{MSYSTEM}" STREQUAL "")
+  find_program (CMAKE_NINJA_BINARY NAMES "ninja")
+  if (CMAKE_NINJA_BINARY)
+    set (
+      CMAKE_GENERATOR "Ninja"
+      CACHE INTERNAL "Cmake generator"
+    )
+    return ()
+  endif ()
+
+  find_program (CMAKE_MAKE_BINARY NAMES "gmake" "make")
+  if (CMAKE_MAKE_BINARY)
+    set (
+      CMAKE_GENERATOR "Unix Makefiles"
+      CACHE INTERNAL "Cmake generator"
+    )
+    return ()
+  endif ()
 endif ()
